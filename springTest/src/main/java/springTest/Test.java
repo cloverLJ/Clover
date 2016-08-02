bde9a74a069102efb976e5725784ac848cd43e82
cloverLJ <feelinglove3286@163.com>
2016-08-02 19:17:02 +0800

    参数初始化

---
+++ springTest/src/main/java/springTest/Test.java
@@ -0,0 +1,11 @@
+package springTest;
+
+import org.springframework.core.AliasRegistry;
+import org.springframework.core.io.ClassPathResource;
+
+public class Test {
+
+	
+	AliasRegistry aa;
+	ClassPathResource bb;
+}
